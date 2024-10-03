a7f25bb4b4da2ac13749ef8b29eadd4b56ccdbec
troubleShooter239 <morgun2282@gmail.com>
2024-10-04 02:33:17 +0300

    project restructure and cmake

--- compile.bat
+++
@@ -1,3 +0,0 @@
-"C:\c-cxx-libs\vulkansdk\Bin\glslc.exe" shaders\shader.vert -o shaders\shader.vert.spv
-"C:\c-cxx-libs\vulkansdk\Bin\glslc.exe" shaders\shader.frag -o shaders\shader.frag.spv
-pause
--- ucompile.sh
+++
@@ -1,2 +0,0 @@
-/usr/bin/glslc shaders/shader.vert -o shaders/shader.vert.spv
-/usr/bin/glslc shaders/shader.frag -o shaders/shader.frag.spv
